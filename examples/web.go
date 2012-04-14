e5ce0f6d54b5a8bc67f380b8f7833229291360b8
Joshua Bussdieker <josh.bussdieker@moovweb.com>
2012-04-14 05:16:42 -0700

    Add web.go example

---
+++ examples/web.go
@@ -0,0 +1,59 @@
+package main
+
+import "fmt"
+import "flag"
+import "html"
+
+import "github.com/hoisie/web"
+import . "github.com/jbussdieker/golibxml"
+
+func testapp(webctx *web.Context, val string) (render string) {
+	render += "<form><textarea style='width:100%; height:288;' name=document>"
+	render += webctx.Params["document"]
+	render += "</textarea><br><input name=xpath value=\"" + webctx.Params["xpath"] + "\"></input><input type=submit></form>"
+
+	// Parse the document
+	doc := ParseHTMLDoc(webctx.Params["document"], "UTF-8")
+	if doc == nil {
+		return
+	}
+	defer doc.Free()
+
+	// Create an xpath context
+	ctx := NewXPathContext(doc.Document)
+	if ctx == nil {
+		return
+	}
+	defer ctx.Free()
+
+	// Evaluate the xpath search
+	result := ctx.Eval(webctx.Params["xpath"])
+	if result == nil {
+		return
+	}
+	defer result.Free()
+
+	// Display results
+	if result.Type() == 1 {
+		nodeset := result.NodeSet()
+		for i := 0; i < nodeset.Size(); i++ {
+			node := nodeset.Item(i)
+			render += fmt.Sprintln("Name:", node.Name(), "<br>")
+			render += fmt.Sprintln("Type:", node.Type(), "<br>")
+			render += fmt.Sprintln("Path:", node.Path(), "<br>")
+			render += fmt.Sprintln("<br><code>", html.EscapeString(node.String()), "</code><br><br>")
+		}
+	} else {
+		render += "Unsupported result type"
+	}
+
+	return
+}
+
+func main() {
+	var port int
+	flag.IntVar(&port, "port", 9999, "Port to run server on")
+	flag.Parse()
+	web.Get("/(.*)", testapp)
+	web.Run(fmt.Sprintf("0.0.0.0:%d", port))
+}
