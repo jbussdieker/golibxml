56683cd3a25cc7c9ee5cf58017976d2017f21c91
Joshua Bussdieker <josh.bussdieker@moovweb.com>
2012-04-14 03:38:53 -0700

    More xpath decorators

--- xpath_wrapper.go
+++ xpath_wrapper.go
@@ -1,9 +1,27 @@
 package golibxml
 
+/*
+#cgo pkg-config: libxml-2.0
+#include <libxml/xpath.h>
+
+xmlNode* fetchNode(xmlNodeSet *nodeset, int index) {
+  	return nodeset->nodeTab[index];
+}
+*/
+import "C"
+
 func (obj *XPathObject) Type() XpathObjectType {
 	return XpathObjectType(obj.Ptr._type)
 }
 
 func (obj *XPathObject) NodeSet() *NodeSet {
 	return makeNodeSet(obj.Ptr.nodesetval)
+}
+
+func (nodeset *NodeSet) Size() int {
+	return int(nodeset.Ptr.nodeNr)
+}
+
+func (nodeset *NodeSet) Item(index int) *Node {
+	return makeNode(C.fetchNode(nodeset.Ptr, C.int(index)))
 }
