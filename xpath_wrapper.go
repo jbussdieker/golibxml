cb6aca553a7bc5afad37c6cb3dea17fbd29411b7
Joshua Bussdieker <josh.bussdieker@moovweb.com>
2012-04-14 06:08:18 -0700

    Forgot to close the channel

--- xpath_wrapper.go
+++ xpath_wrapper.go
@@ -14,11 +14,13 @@ func (obj *XPathObject) Results() chan *Node {
 	channel := make(chan *Node)
 	go func(obj *XPathObject, channel chan *Node) {
 		if obj.Ptr._type != 1 {
+			close(channel)
 			return
 		}
 		for i := 0; i < int(obj.Ptr.nodesetval.nodeNr); i++ {
 			channel <- makeNode(C.fetchNode(obj.Ptr.nodesetval, C.int(i)))
 		}
+		close(channel)
 	}(obj, channel)
 	return channel
 }
