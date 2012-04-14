606bd262c2ef1dab48c48976dd4baf99ef22a039
Joshua Bussdieker <josh.bussdieker@moovweb.com>
2012-04-14 03:13:02 -0700

    Helper function

--- xpath_wrapper.go
+++ xpath_wrapper.go
@@ -3,3 +3,7 @@ package golibxml
 func (obj *XPathObject) Type() XpathObjectType {
 	return XpathObjectType(obj.Ptr._type)
 }
+
+func (obj *XPathObject) NodeSet() *NodeSet {
+	return makeNodeSet(obj.Ptr.nodesetval)
+}
